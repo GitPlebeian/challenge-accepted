bdc5a1ede71ca726c512a83b973c4813c6ec646c
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-10-04 08:42:04 -0600

    tabcontrollerstuff

---
+++ ChallengeMe/ChallengeMe/View Controllers/Navigation View Controllers/MainTabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabBarViewController.swift
+//  ChallengeMe
+//
+//  Created by Jackson Tubbs on 10/4/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
