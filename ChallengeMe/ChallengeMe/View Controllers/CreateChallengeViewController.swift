1cbc7fac35a52db14111af803da86bb8515b7a4a
Mike Moore <michaelhmoore1@gmail.com>
2019-10-07 12:23:51 -0600

    remote notifications plist, image and location selection update

--- ChallengeMe/ChallengeMe/View Controllers/CreateChallengeViewController.swift
+++ ChallengeMe/ChallengeMe/View Controllers/CreateChallengeViewController.swift
@@ -11,6 +11,11 @@ import Photos
 import CoreLocation
 import AVFoundation
 
+protocol PhotoSelectedDelegate: class {
+    // needed to get back the image from photo library or camera
+    func photoSelected(image: UIImage)
+}
+
 class CreateChallengeViewController: UIViewController {
     
     // MARK: - Properties
@@ -31,20 +36,20 @@ class CreateChallengeViewController: UIViewController {
     @IBOutlet weak var minutesLabel: UILabel!
     @IBOutlet weak var secondsTextField: UITextField!
     @IBOutlet weak var secondsLabel: UILabel!
+    @IBOutlet weak var selectedImage: UIImageView!
     @IBOutlet weak var createChallengeButton: UIButton!
     
     let locationManager = CLLocationManager()
     var timer = false
     var countDown = false
     var challengeLocation: CLLocationCoordinate2D?
-    var challengeImage: UIImage?
-    let captureSession = AVCaptureSession()
+    
+    weak var delegate: PhotoSelectedDelegate?
     
     // MARK: - Lifecycle Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         loadViews()
-        
     }
     
     // MARK: - Actions
@@ -86,16 +91,14 @@ class CreateChallengeViewController: UIViewController {
             let description = descriptionTextView.text,
             let location = challengeLocation,
             let measurement = measurementTextField.text,
-            let challengeImage = challengeImage,
+            let challengeImage = selectedImage.image,
             let tagString = tagsTextField.text else { return }
-        
         var hashtags: [String] {
             return tagString
-            .split(separator: " ") // divide into 'substrings'
+                .split(separator: " ") // divide into 'substrings'
                 .map { String($0) } // turn back into 'strings'
                 .filter { $0.hasPrefix("#") } // only keep #strings
         }
-        
         ChallengeController.shared.createChallenge(title: title, description: description, measurement: measurement, longitude: location.longitude, latitude: location.latitude, tags: hashtags, photo: challengeImage) { (success) in
             if success {
                 print("A challenge was saved")
@@ -111,6 +114,7 @@ class CreateChallengeViewController: UIViewController {
         timerSwitch.setOn(false, animated: true)
         toggleTimerView()
         toggleCountDownView()
+        selectedImage.isHidden = false
     }
     
     func toggleTimerView() {
@@ -137,23 +141,23 @@ class CreateChallengeViewController: UIViewController {
     }
     
     func toggleCountDownView() {
-            UIView.animate(withDuration: 0.2) {
-                if self.countDown == true {
-                    self.hoursTextField.isHidden = false
-                    self.hoursLabel.isHidden = false
-                    self.minutesTextField.isHidden = false
-                    self.minutesLabel.isHidden = false
-                    self.secondsTextField.isHidden = false
-                    self.secondsLabel.isHidden = false
-                } else {
-                    self.hoursTextField.isHidden = true
-                    self.hoursLabel.isHidden = true
-                    self.minutesTextField.isHidden = true
-                    self.minutesLabel.isHidden = true
-                    self.secondsTextField.isHidden = true
-                    self.secondsLabel.isHidden = true
-                }
+        UIView.animate(withDuration: 0.2) {
+            if self.countDown == true {
+                self.hoursTextField.isHidden = false
+                self.hoursLabel.isHidden = false
+                self.minutesTextField.isHidden = false
+                self.minutesLabel.isHidden = false
+                self.secondsTextField.isHidden = false
+                self.secondsLabel.isHidden = false
+            } else {
+                self.hoursTextField.isHidden = true
+                self.hoursLabel.isHidden = true
+                self.minutesTextField.isHidden = true
+                self.minutesLabel.isHidden = true
+                self.secondsTextField.isHidden = true
+                self.secondsLabel.isHidden = true
             }
+        }
     }
     
     fileprivate func presentPhotoPickerController() {
@@ -165,59 +169,32 @@ class CreateChallengeViewController: UIViewController {
         }
     }
     
-    fileprivate func requestPhotoLibraryAuthorization() {
-           // request authorization to access photos
-           PHPhotoLibrary.requestAuthorization { (status) in
-               switch status {
-               case .authorized:
-                   self.presentPhotoPickerController()
-               case .notDetermined:
-                   if status == PHAuthorizationStatus.authorized {
-                       self.presentPhotoPickerController()
-                   }
-               case .restricted:
-                   let alert = UIAlertController(title: "Photo Library Restricted", message: "Photo Library access is restricted and cannot be accessed", preferredStyle: .alert)
-                   let okay = UIAlertAction(title: "Ok", style: .default)
-                   alert.addAction(okay)
-                   self.present(alert, animated: true)
-               case .denied:
-                   let alert = UIAlertController(title: "Photo Library Denied", message: "Photo Library access was previously denied.  Please update your Settings.", preferredStyle: .alert)
-                   let settings = UIAlertAction(title: "Settings", style: .default) { (action) in
-                       DispatchQueue.main.async {
-                           if let url = URL(string: UIApplication.openSettingsURLString) {
-                               UIApplication.shared.open(url, options: [:])
-                           }
-                       }
-                   }
-                   let cancel = UIAlertAction(title: "Cancel", style: .cancel)
-                   alert.addAction(settings)
-                   alert.addAction(cancel)
-                   self.present(alert, animated: true)
-               }
-           }
-       }
-    
-    func setupCaptureSession() {
-        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+    func presentCamera() {
+        DispatchQueue.main.async {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self
+            imagePickerController.sourceType = .camera
+            self.present(imagePickerController, animated: true)
+        }
     }
     
-    fileprivate func requestCameraAuthorization() {
-            switch AVCaptureDevice.authorizationStatus(for: .video) {
+    fileprivate func requestPhotoLibraryAuthorization() {
+        // request authorization to access photos
+        PHPhotoLibrary.requestAuthorization { (status) in
+            switch status {
             case .authorized:
-                self.setupCaptureSession()
+                self.presentPhotoPickerController()
             case .notDetermined:
-                AVCaptureDevice.requestAccess(for: .video) { (granted) in
-                    if granted {
-                        self.setupCaptureSession()
-                    }
+                if status == PHAuthorizationStatus.authorized {
+                    self.presentPhotoPickerController()
                 }
             case .restricted:
-                let alert = UIAlertController(title: "Camera Restricted", message: "Camera access is restricted and cannot be accessed", preferredStyle: .alert)
+                let alert = UIAlertController(title: "Photo Library Restricted", message: "Photo Library access is restricted and cannot be accessed", preferredStyle: .alert)
                 let okay = UIAlertAction(title: "Ok", style: .default)
                 alert.addAction(okay)
                 self.present(alert, animated: true)
             case .denied:
-                let alert = UIAlertController(title: "Camera Denied", message: "Camera access was previously denied.  Please update your Settings.", preferredStyle: .alert)
+                let alert = UIAlertController(title: "Photo Library Denied", message: "Photo Library access was previously denied.  Please update your Settings.", preferredStyle: .alert)
                 let settings = UIAlertAction(title: "Settings", style: .default) { (action) in
                     DispatchQueue.main.async {
                         if let url = URL(string: UIApplication.openSettingsURLString) {
@@ -231,10 +208,41 @@ class CreateChallengeViewController: UIViewController {
                 self.present(alert, animated: true)
             }
         }
+    }
+    
+    fileprivate func requestCameraAuthorization() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            self.presentCamera()
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { (granted) in
+                if granted {
+                    self.presentCamera()
+                }
+            }
+        case .restricted:
+            let alert = UIAlertController(title: "Camera Restricted", message: "Camera access is restricted and cannot be accessed", preferredStyle: .alert)
+            let okay = UIAlertAction(title: "Ok", style: .default)
+            alert.addAction(okay)
+            self.present(alert, animated: true)
+        case .denied:
+            let alert = UIAlertController(title: "Camera Denied", message: "Camera access was previously denied.  Please update your Settings.", preferredStyle: .alert)
+            let settings = UIAlertAction(title: "Settings", style: .default) { (action) in
+                DispatchQueue.main.async {
+                    if let url = URL(string: UIApplication.openSettingsURLString) {
+                        UIApplication.shared.open(url, options: [:])
+                    }
+                }
+            }
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            alert.addAction(settings)
+            alert.addAction(cancel)
+            self.present(alert, animated: true)
+        }
+    }
 }
 
 extension CreateChallengeViewController: CreateChallengeMapDelegate {
-    
     func didTap(at coordinates: CLLocationCoordinate2D) {
         challengeLocation = coordinates
     }
@@ -243,7 +251,9 @@ extension CreateChallengeViewController: CreateChallengeMapDelegate {
 extension CreateChallengeViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            self.challengeImage = selectedImage
+            delegate?.photoSelected(image: selectedImage)
+            self.selectedImage.image = selectedImage
+            self.selectedImage.isHidden = false
         }
         dismiss(animated: true)
     }
